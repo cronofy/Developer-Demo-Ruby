cceda162e99fe5d4638b9fe10f59cd1b51604029
Shannon Walker <shannon@cronofy.com>
2021-02-01 16:25:50 +0000

    clean up some unneccessary code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,22 +22,15 @@ class ApplicationController < ActionController::Base
   private
 
   def refresh_access_token
-    client_id =     Rails.application.credentials[:cronofy][:client_id]
-    client_secret = Rails.application.credentials[:cronofy][:client_secret]
-
     @user = User.first
-
     @cronofy = cronofy_client(@user)
-
     response = @cronofy.refresh_access_token
 
     if User.where(refresh_token: response.refresh_token).exists?
       user = User.find_by(refresh_token: response.refresh_token)
       user.update(access_token: response.access_token)
       user.save
-      redirect_to root_path
-    else
-      redirect_to root_path
     end
+    redirect_to root_path
   end
 end
